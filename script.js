7e2f556c4a8622c6e9d1d26047830fa0b027d8fe
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-22 22:09:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([37.76,-122.45], 12);
+var map = L.map('map').setView([40.3,-94.1], 8);
 
   // load a tile layer
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
