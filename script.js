f36c39aac61180a0371016ff6a481dce31feb078
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-26 20:59:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/***************************
+**
+**	LEAFLET
+**
+***************************/
 
   // load Google Satellite
 var sat = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
@@ -114,3 +119,47 @@ map.on('zoomend', function() {
 		L.control.layers(contour_layer).removeFrom(map);
 	}
 });*/
+
+/***************************
+**
+**	ArcGIS 2D API
+**
+***************************/
+
+
+ require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/FeatureLayer"
+      ], function (Map, MapView, FeatureLayer) {
+        var map = new Map({
+          basemap: "hybrid"
+        });
+
+        var view = new MapView({
+          container: "viewDiv",
+          map: map,
+
+          extent: {
+            // autocasts as new Extent()
+            xmin: -10366387,
+            ymin: 4902434,
+            xmax: -10356735,
+            ymax: 4919960,
+            spatialReference: 102100
+          }
+        });
+
+        /********************
+         * Add feature layer
+         ********************/
+
+        var featureLayer1 = new FeatureLayer({     url:"https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/Commission_prop_footprint/FeatureServer"});
+   
+        map.add(featureLayer1);
+   
+   var featureLayer2 = new FeatureLayer({     url:"https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/Normal_Pool/FeatureServer"});
+   
+        map.add(featureLayer2);
+      });
+
