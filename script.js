cf8c91e48cf74f56a808061d08e702ae117678d8
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-27 20:39:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -236,7 +236,8 @@ setTimeout(function(){
 	 var arc_2d_layerListExpand = new Expand({
 		//  expandIconClass: "esri-icon-layer-list",  
 		  view: arc_2d_view,
-		  content: arc_2d_basemapLayerList
+  		content: arc_2d_basemapLayerList.domNode
+		//  content: arc_2d_basemapLayerList
 		});
 	arc_2d_view.ui.add(arc_2d_layerListExpand, "top-right");
 	 
